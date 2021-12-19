55b26a4e8ebb5663b79e2f29f58f4d7b1b7e5b94
kastona <stephenkastona@gmail.com>
2021-12-19 13:35:08 +0100

    greatest

--- .nuxt/index.js
+++ .nuxt/index.js
@@ -14,13 +14,13 @@ import { createStore } from './store.js'
 /* Plugins */
 
 import nuxt_plugin_plugin_61b066ba from 'nuxt_plugin_plugin_61b066ba' // Source: ./components/plugin.js (mode: 'all')
-import nuxt_plugin_plugin_1c9ce774 from 'nuxt_plugin_plugin_1c9ce774' // Source: ./vuetify/plugin.js (mode: 'all')
 import nuxt_plugin_moment_39c95a70 from 'nuxt_plugin_moment_39c95a70' // Source: ./moment.js (mode: 'all')
 import nuxt_plugin_workbox_3b58d9ec from 'nuxt_plugin_workbox_3b58d9ec' // Source: ./workbox.js (mode: 'client')
 import nuxt_plugin_metaplugin_54411602 from 'nuxt_plugin_metaplugin_54411602' // Source: ./pwa/meta.plugin.js (mode: 'all')
 import nuxt_plugin_axios_ea094e60 from 'nuxt_plugin_axios_ea094e60' // Source: ./axios.js (mode: 'all')
 import nuxt_plugin_toast_3357971e from 'nuxt_plugin_toast_3357971e' // Source: ../plugins/toast (mode: 'client')
 import nuxt_plugin_social_3b037b3a from 'nuxt_plugin_social_3b037b3a' // Source: ../plugins/social (mode: 'client')
+import nuxt_plugin_vuetify_165b1c58 from 'nuxt_plugin_vuetify_165b1c58' // Source: ../plugins/vuetify (mode: 'all')
 import nuxt_plugin_plugin_031eebe8 from 'nuxt_plugin_plugin_031eebe8' // Source: ./auth/plugin.js (mode: 'all')
 
 // Component: <ClientOnly>
@@ -61,7 +61,7 @@ Object.defineProperty(Vue.prototype, '$nuxt', {
 
 Vue.use(Meta, {"keyName":"head","attribute":"data-n-head","ssrAttribute":"data-n-head-ssr","tagIDKeyName":"hid"})
 
-const defaultTransition = {"name":"page","mode":"out-in","appear":false,"appearClass":"appear","appearActiveClass":"appear-active","appearToClass":"appear-to"}
+const defaultTransition = {"name":"page","mode":"out-in","appear":true,"appearClass":"appear","appearActiveClass":"appear-active","appearToClass":"appear-to"}
 
 const originalRegisterModule = Vuex.Store.prototype.registerModule
 
@@ -81,15 +81,12 @@ async function createApp(ssrContext, config = {}) {
   // Add this.$router into store actions/mutations
   store.$router = router
 
-  // Fix SSR caveat https://github.com/nuxt/nuxt.js/issues/3757#issuecomment-414689141
-  store.registerModule = registerModule
-
   // Create Root instance
 
   // here we inject the router and store to all child components,
   // making them available everywhere as `this.$router` and `this.$store`.
   const app = {
-    head: {"titleTemplate":"%s - Nuxt js UI Kit","title":"NuxtJs UI kit | Free UI kit built with Vuetifyy","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""},{"name":"format-detection","content":"telephone=no"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"},{"rel":"stylesheet","type":"text\u002Fcss","href":"https:\u002F\u002Ffonts.googleapis.com\u002Fcss?family=Roboto:100,300,400,500,700,900&display=swap"},{"rel":"stylesheet","type":"text\u002Fcss","href":"https:\u002F\u002Fcdn.jsdelivr.net\u002Fnpm\u002F@mdi\u002Ffont@latest\u002Fcss\u002Fmaterialdesignicons.min.css"}],"style":[],"script":[]},
+    head: {"titleTemplate":"%s - Nuxt js UI Kit","title":"NuxtJs UI kit | Free UI kit built with Vuetifyy","htmlAttrs":{"lang":"en"},"meta":[{"charset":"utf-8"},{"name":"viewport","content":"width=device-width, initial-scale=1"},{"hid":"description","name":"description","content":""},{"name":"format-detection","content":"telephone=no"}],"link":[{"rel":"icon","type":"image\u002Fx-icon","href":"\u002Ffavicon.ico"}],"style":[],"script":[]},
 
     store,
     router,
@@ -222,10 +219,6 @@ async function createApp(ssrContext, config = {}) {
     await nuxt_plugin_plugin_61b066ba(app.context, inject)
   }
 
-  if (typeof nuxt_plugin_plugin_1c9ce774 === 'function') {
-    await nuxt_plugin_plugin_1c9ce774(app.context, inject)
-  }
-
   if (typeof nuxt_plugin_moment_39c95a70 === 'function') {
     await nuxt_plugin_moment_39c95a70(app.context, inject)
   }
@@ -248,6 +241,10 @@ async function createApp(ssrContext, config = {}) {
 
   if (process.client && typeof nuxt_plugin_social_3b037b3a === 'function') {
     await nuxt_plugin_social_3b037b3a(app.context, inject)
+  }
+
+  if (typeof nuxt_plugin_vuetify_165b1c58 === 'function') {
+    await nuxt_plugin_vuetify_165b1c58(app.context, inject)
   }
 
   if (typeof nuxt_plugin_plugin_031eebe8 === 'function') {
